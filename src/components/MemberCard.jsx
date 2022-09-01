67579c63280995daab93306e234d5841e91cc687
R.V.Rajagopalan <rajagopalan@RVRajagopalans-MacBook-Pro.local>
2022-09-01 22:51:30 +0530

    mohan is everywhere

--- src/components/MemberCard.jsx
+++ src/components/MemberCard.jsx
@@ -1,29 +1,19 @@
 import React from 'react'
-import Mon from '../assets/mon.jpeg'
 
-export default function MemberCard() {
+export default function MemberCard(props) {
     return (
         <>
             <div className='ml-3 mr-3 grid grid-cols-1'>
-                <div className="rounded-3xl overflow-hidden shadow-xl max-w-xs my-3 bg-indigo-100">
-                    {/* <img src={Mon} className="w-full h-[125px]" /> */}
-                    <div className="w-full h-[125px] bg-[#006979]"></div>
+                <div className="rounded-3xl overflow-hidden shadow-xl max-w-xs my-3 bg-gradient-to-t from-[#006979] to-[#000B4B]">
+                    <div className="w-full h-[125px] bg-white"></div>
                     <div className="flex justify-center -mt-[100px]">
-                        <img src={Mon} className="rounded-full border-solid border-white border-2 -mt-3" />
+                        <img src={props.profilePic} className="w-[200px] h-[200px] rounded-full border-solid border-white border-2 -mt-3" />
                     </div>
                     <div className="text-center px-3 pb-6 pt-2">
-                        <h3 className="text-sm bold font-sans">Mohan Swain</h3>
-                        <p className="mt-2 font-sans font-light">Hello, i'm from another the other side!</p>
+                        <h3 className="font-poppins text-2xl font-semibold mt-5">{props.name}</h3>
+                        <p className="mt-2 font-poppins font-semibold">{props.fields}</p>
                     </div>
                     <div className="flex justify-center pb-3">
-                        {/* <div className="text-center mr-3 border-r pr-3">
-                            <h2>34</h2>
-                            <span>Photos</span>
-                        </div> */}
-                        {/* <div className="text-center">
-                            <h2>42</h2>
-                            <span>Friends</span>
-                        </div> */}
                     </div>
                 </div>
             </div>
--- src/components/OurTeam.jsx
+++ src/components/OurTeam.jsx
@@ -1,37 +1,21 @@
 import React from "react";
 import MemberCard from "./MemberCard";
-// import ProfileCard from "./ProfileCard";
-import Sadasya from '../../sadasya.json'
+import HomepageAlumni from '../../json/HomepageAlumni.json'
 
 export default function OurTeam() {
-
-  const saareSadasya = Sadasya.map((sadasya) => {
-    return (
-      <MemberCard
-        key={sadasya.id}
-        name={sadasya.name}
-        designation={sadasya.designation}
-        image={sadasya.image}
-        description={sadasya.description}
-      />
-    );
-  })
 
   return (
     <div>
       <div>
-
       </div>
       <h1 className="text-white text-4xl font-extrabold mb-10 font-poppins justify-start">
         Our Team
       </h1>
 
-      <div className="grid grid-cols-3">
-        {/* <MemberCard />
-        <MemberCard />
-        <MemberCard />
-        <MemberCard /> */}
-        {saareSadasya}
+      <div className="grid grid-cols-4">
+        {HomepageAlumni.map((member, index) => {
+          return <MemberCard key={index} name={member.name} fields={member.fields} profilePic={member.image} />
+        })}
       </div>
     </div>
   );
