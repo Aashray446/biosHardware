32c8ec4d3dfbc7248c4b9932b086ee836f02fffe
Anshuman <anshuman8swain@gmail.com>
2022-09-02 08:13:03 +0530

    rid1dle's modifications done

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -31,92 +31,103 @@ const CardContainer = styled.div`
   }
   hr {
     width:3.5rem;
-    }
-    .grounded-radiants {
-        position: relative;
-        border: 4px solid transparent;
-        border-radius: 20px;
-        background: linear-gradient(to right, #BB70DE, #E68E84);
-        background-clip: padding-box;
-        padding: 2px;
-    }
+  }
+  .grounded-radiants {
+    position: relative;
+    border: 4px solid transparent;
+    border-radius: 20px;
+    background: linear-gradient(to right, #BB70DE, #E68E84);
+    background-clip: padding-box;
+    padding: 2px;
+  }
     
-    .grounded-radiants::after {
-        position: absolute;
-        top: -4px; bottom: -4px;
-        left: -4px; right: -4px;
-        background: linear-gradient(to right, #BB70DE, #E68E84);
-        content: '';
-        z-index: -1;
-        border-radius: 16px;
-    }
-    section {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        text-align: left;
-        font-size: 1.5em;
-        color: rgb(88 199 250 / 100%);
-        margin: 3rem 3rem 1rem 3rem;
-    }
-    h2 {
-        font-size: 1.5rem;
-        color:#fff;
-        font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-        font-weight: bold;
-        margin-top: 1rem;
-        margin-left: 0.1875rem;
-
-    }
-    .small-container {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        margin: 0 auto;
-    }
-    .headline-text {
-        margin-left: 5rem;
-        font-size: 1rem;
-        color: #B5B6B8; !important;
-    }
-    span {
-        margin-top: 2em;
-        margin-bottom: 2em;
-        display: block; 
-      }
-    h1 {
-        font-size: 2.125rem;
-        font-weight: bold;
-        font-family: Arial, 
-            Helvetica, sans-serif;
-        background: linear-gradient(to right, #BB70DE, #E68E84);
-        -webkit-text-fill-color: transparent;
-        -webkit-background-clip: text;
-    }
-    .side-text {
-        margin-left: 40rem;
-        font-size: 1.25rem;
-        text-align: right;
-        color: #E68E84; !important;
-    }
-    .grid-container {
-        display: grid;
-        grid-template-columns: auto auto auto;
-    }
-    .grid-item {
-        display: flex;
-        flex-direction: column;
-        font-size: 30px;
-        text-align: left;
-        padding-bottom: 2rem;
-        padding-right: 2rem;
-    }
-    .grid-item-text {
-        font-size: 1rem;
-        color: #81AFDD;
-        font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-        margin-top: 3rem;
-    }
+  .grounded-radiants::after {
+    position: absolute;
+    top: -4px; bottom: -4px;
+    left: -4px; right: -4px;
+    background: linear-gradient(to right, #BB70DE, #E68E84);
+    content: '';
+    z-index: -1;
+    border-radius: 16px;
+  }
+  section {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    text-align: left;
+    font-size: 1.5em;
+    color: rgb(88 199 250 / 100%);
+    margin: 3rem 3rem 1rem 3rem;
+  }
+  h2 {
+    font-size: 1.5rem;
+    color:#fff;
+    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+    font-weight: bold;
+    margin-top: 1rem;
+    margin-left: 0.1875rem;
+  }
+  .small-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 0 auto;
+  }
+  .headline-text {
+    margin-left: 5rem;
+    font-size: 1rem;
+    color: #B5B6B8; !important;
+  }
+  span {
+    margin-top: 2em;
+    margin-bottom: 2em;
+    display: block; 
+  }
+  h1 {
+    font-size: 2.125rem;
+    font-weight: bold;
+    font-family: Arial, 
+        Helvetica, sans-serif;
+    background: linear-gradient(to right, #BB70DE, #E68E84);
+    -webkit-text-fill-color: transparent;
+    -webkit-background-clip: text;
+  }
+  .side-text {
+    font-size: 1.25rem;
+    text-align: right;
+    color: #E68E84; !important;
+    margin-right: 1.5rem;
+  }
+  .grid-container {
+    display: grid;
+    grid-template-columns: auto auto auto;
+  }
+  .grid-item {
+    display: flex;
+    flex-direction: column;
+    font-size: 30px;
+    text-align: left;
+    padding-bottom: 2rem;
+    padding-right: 2rem;
+  }
+  .grid-item-text {
+    font-size: 1rem;
+    color: #81AFDD;
+    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+    margin-top: 3rem;
+  }
+  .align-right {
+    width: 50%;
+    text-align: right;
+  }
+  .align-left {
+    width: 50%;
+    text-align: left;
+  }
+  .side-text:hover {
+    color: rgb(14 116 144 / 100%);
+    text-decoration: underline;
+    cursor: pointer;
   }
 `;
 const InfoCard = () => {
@@ -136,9 +147,15 @@ const InfoCard = () => {
         </section>
         <span></span>
         <section>
-          <h1>Our Fields of Research</h1>
-          {/* <div className="side-text">Explore the library</div> */}
-          <button class="rounded-full bg-transparent border-2 text-end ml-[35rem] p-[1rem] text-lg border-cyan-700" onClick={"https://wiki.bi0s.in/"}>Explore The Library</button>
+        <h1 className="align-left">Our Fields of Research</h1>
+          <div
+            className="side-text align-right"
+            onClick={() => {
+              window.open("https://wiki.bi0s.in/", "_blank");
+            }}
+          >
+            Explore the library
+          </div>
         </section>
         <span></span>
         <section>
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -10,27 +10,27 @@ const Line = styled.div`
     border-radius: 5px;
     height: 15px;
     left: 50%;
-    margin-left: 6rem;
+    margin-top: 1rem;
+    margin-left: 1rem;
   }
 `;
 
 const Stats = () => (
   <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
     {stats.map((stat, index) => (
+      <div key={stat.id} className="flex justify-evenly">
       <div
-        key={stat.id}
         className={`flex-1 flex justify-center items-center flex-row m-3 `}
       >
         <p className="flex font-poppins font-normal xs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px] text-gradient uppercase ml-3">
           {stat.title}
-          <span
-            className={index === stats.length - 1 ? "invisible" : "visible"}
-          >
-            <Line>
-              <div className="vertical"></div>
-            </Line>
-          </span>
         </p>
+      </div>
+      <span className={index === stats.length - 1 ? "invisible" : "visible"}>
+        <Line>
+          <div className="vertical"></div>
+        </Line>
+      </span>
       </div>
     ))}
   </section>
