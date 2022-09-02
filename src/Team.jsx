64a7908377c9932096af38937dc1ab7a11df634b
Himanshu Lath <lathhimanshu@gmail.com>
2022-09-02 15:10:35 +0530

    Fix: Overflow in right

--- src/Team.jsx
+++ src/Team.jsx
@@ -8,8 +8,9 @@ function Team() {
       <TeamPage />
       {/* gradient start */}
       <div className="absolute z-[-0] w-[20%] h-[35%] top-[100vh] cyan__gradient" />
-      <div className="absolute z-[0] w-[40%] h-[40%] top-24 left-[75vw] rounded-full white__gradient " />
-      <div className="absolute z-[0] w-[30%] h-[40%] top-28 left-[75vw] rounded-full blue__gradient " />
+      {/* This caused overflow on the right so I commented it out */}
+      {/* <div className="absolute z-[0] w-[40%] h-[40%] top-24 left-[75vw] rounded-full white__gradient " />
+      <div className="absolute z-[0] w-[30%] h-[40%] top-28 left-[75vw] rounded-full blue__gradient " /> */}
       <div className="absolute z-[0] w-[40%] h-[40%] top-[185vh] left-[10vw] rounded-full white__gradient " />
       <div className="absolute z-[0] w-[30%] h-[40%] top-[190vh] left-[10vw] rounded-full blue__gradient " />
       <div className="absolute z-[0] w-[25%] h-[50%] right-20 top-[105vh] blue__gradient" />
