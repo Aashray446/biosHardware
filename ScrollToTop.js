d85bd008713225427d5a51363b58f9ecbf168ea1
Himanshu Lath <lathhimanshu@gmail.com>
2022-09-03 01:21:02 +0530

    Fix: Scroll to top not working on fireforx

--- ScrollToTop.js
+++ ScrollToTop.js
@@ -4,7 +4,7 @@ import { useLocation } from "react-router-dom";
 export default function ScrollToTop() {
   const { pathname } = useLocation();
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo(1, 1);
   }, [pathname]);
 
   return null;
