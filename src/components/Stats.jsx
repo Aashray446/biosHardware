c61b6a041836d54bc85663e75bb996b17ea0615e
Himanshu Lath <lathhimanshu@gmail.com>
2022-09-04 18:58:54 +0530

    Fix: Made the stat bar a bit better in small view

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -13,25 +13,29 @@ const Line = styled.div`
     margin-top: 1rem;
     margin-left: 1rem;
   }
+  @media only screen and (max-width: 570px) {
+    .vertical {
+      visibility: hidden;
+    }
+  }
 `;
 
 const Stats = () => (
   <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6 `}>
     {stats.map((stat, index) => (
       <div key={stat.id} className="flex justify-evenly">
-      <div
-        className={`flex-1 flex justify-center items-center flex-row m-3 `}
-      >
-        <p className="flex font-poppins font-normal xs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px] text-gradient uppercase ml-3">
-          {stat.title}
-        </p>
-
-      </div>
-      <span className={index === stats.length - 1 ? "invisible" : "visible"}>
-        <Line>
-          <div className="vertical"></div>
-        </Line>
-      </span>
+        <div
+          className={`flex-1 flex justify-center items-center flex-row m-3 `}
+        >
+          <p className="flex font-poppins font-normal xs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px] text-gradient uppercase ml-3">
+            {stat.title}
+          </p>
+        </div>
+        <span className={index === stats.length - 1 ? "invisible" : "visible"}>
+          <Line>
+            <div className="vertical"></div>
+          </Line>
+        </span>
       </div>
     ))}
   </section>
