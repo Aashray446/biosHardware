a3471b6fd249072a630ff7ede453b07d54fa0b12
Himanshu Lath <lathhimanshu@gmail.com>
2022-09-08 21:34:06 +0530

    Fix for "Scroll Hoja" for Firefox

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,22 +10,34 @@ import {
 } from "./components";
 import OurTeam from "./components/OurTeam";
 
-
 function Home() {
-
   const scrollHoja = () => {
     window.scrollTo({
-      top: 0,
-      left: 0,
+      top: 1,
+      left: 1,
       behavior: "smooth",
     });
-  }
+  };
 
   return (
     <div>
-      <button className="fixed text-white right-8 bottom-8 bg-black p-3 rounded-lg z-[55] cursor-pointer" onClick={() => scrollHoja()}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={5} stroke="currentColor" className="w-9 h-9">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+      <button
+        className="fixed text-white right-8 bottom-8 bg-black p-3 rounded-lg z-[55] cursor-pointer"
+        onClick={() => scrollHoja()}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={5}
+          stroke="currentColor"
+          className="w-9 h-9"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.5 15.75l7.5-7.5 7.5 7.5"
+          />
         </svg>
       </button>
 
@@ -45,4 +57,3 @@ function Home() {
 }
 
 export default Home;
-
