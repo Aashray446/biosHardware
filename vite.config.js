fa8f94707d052f9326db985fca155ce1b4d18ec7
Anshuman <anshuman8swain@gmail.com>
2022-09-09 20:21:18 +0530

    alumni data added

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/bioshardware/",
+  // base: "",
   plugins: [
     react({
       jsxRuntime: "classic",
